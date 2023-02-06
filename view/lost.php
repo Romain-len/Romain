56d32a00c4e45e887983794dae524332795d890f
Romain Lenoir <89740581+romain-lnr@users.noreply.github.com>
2023-02-06 09:58:30 +0100

    feat:lost

--- view/lost.php
+++ view/lost.php
@@ -14,7 +14,7 @@ $title="RentASnow - Perdu";
 <div class="span12" id="divMain">
   <h1>Oupssss... vous vous êtes perdu ;(</h1>
 
-    <a href="index.php?action=home">Retourner à l'accueil</a>
+    <a href="../index.php?action=home">Retourner à l'accueil</a>
 
 
 <?php
